8f35279690c0151b43f1e1a52e452e025d6559ff
Guruhg19 <guruhvitra19@gmail.com>
2025-04-19 14:55:41 +0700

    Implement repositories for Booking, City, Gym, and SubscribePackage with interfaces for improved data handling

---
+++ app/Repositories/CityRepository.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Repositories;
+
+use App\Models\City;
+use App\Repositories\Contracts\CityRepositoryInterface;
+
+class CityRepository implements CityRepositoryInterface
+{
+    public function getAllCities()
+    {
+        return City::latest()->get();
+    }
+}
