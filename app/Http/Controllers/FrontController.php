39fc4698923d950065157f280a008249c6b0f7d9
Guruhg19 <guruhvitra19@gmail.com>
2025-04-20 12:57:02 +0700

    Refactor FrontController to integrate FrontService for improved data handling and add methods for front page, pricing, details, and city views.

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,9 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Gym;
+use App\Models\City;
 use Illuminate\Http\Request;
+use App\Services\FrontService;
 
 class FrontController extends Controller
 {
-    //
+
+    protected $frontService;
+
+    public function __construct(FrontService $frontService)
+    {
+        $this->frontService = $frontService;
+    }
+
+
+    public function index()
+    {
+        $data = $this->frontService->getFrontPageData();
+        // dd($data);
+        // return view('front.index', compact('data'));
+    }
+
+    public function pricing()
+    {
+        $data = $this->frontService->getSubscriptionsData();
+        // dd($data);
+        // return view('front.pricing', compact('data'));
+    }
+
+    public function details(Gym $gym)
+    {
+        // dd($gym);
+        // return view('front.details', compact('gym'));
+    }
+
+    public function city(City $city)
+    {
+        // dd($city);
+        // return view('front.city', compact('city'));
+    }
+
 }
